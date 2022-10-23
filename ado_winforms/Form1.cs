6029ff277dbf8effc401efb210744e676f81c0e6
hoshiboychick <scherbodenis@gmail.com>
2022-10-23 11:48:25 +0300

    Add Windows Forms ADO.NET project

---
+++ ado_winforms/Form1.cs
@@ -0,0 +1,47 @@
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ado_winforms
+{
+    public partial class Form1 : Form
+    {
+        string connectionString;
+        SqlDataAdapter adapter;
+        DataTable usersTable;
+
+        public Form1()
+        {
+            InitializeComponent();
+            connectionString = ConfigurationManager.ConnectionStrings["ConnectionLocalDb"].ToString();
+
+            string sqlExpression = "SELECT * FROM Users";
+            usersTable = new DataTable();
+            SqlConnection connection = null;
+            try
+            {
+                connection = new SqlConnection(connectionString);
+                SqlCommand command = new SqlCommand(sqlExpression, connection);
+                adapter = new SqlDataAdapter(command);
+
+                connection.Open();
+                adapter.Fill(usersTable);
+                dataGridView1.DataSource = usersTable.DefaultView;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (connection != null)
+                    connection.Close();
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
